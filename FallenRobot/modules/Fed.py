df609e145d981996be8ed71034121ad19e3cd33c
Devilking12 <128044704+Devilking12@users.noreply.github.com>
2023-11-19 15:40:09 +0530

    Update Fed.py
    Signed-off-by: Devilking12 <128044704+Devilking12@users.noreply.github.com>

--- FallenRobot/modules/Fed.py
+++ FallenRobot/modules/Fed.py
@@ -18,8 +18,8 @@ from telegram.error import BadRequest, TelegramError, Unauthorized
 from telegram.ext import CallbackContext, CallbackQueryHandler, CommandHandler
 from telegram.utils.helpers import mention_html, mention_markdown
 
-import MukeshRobot.modules.sql.feds_sql as sql
-from MukeshRobot import (
+import FallenRobot.modules.sql.feds_sql as sql
+from FallenRobot import (
     DRAGONS,
     EVENT_LOGS,
     LOGGER,
@@ -29,15 +29,15 @@ from MukeshRobot import (
     WOLVES,
     dispatcher,
 )
-from MukeshRobot.modules.disable import DisableAbleCommandHandler
-from MukeshRobot.modules.helper_funcs.alternate import send_message
-from MukeshRobot.modules.helper_funcs.chat_status import is_user_admin
-from MukeshRobot.modules.helper_funcs.extraction import (
+from FallenRobot.modules.disable import DisableAbleCommandHandler
+from FallenRobot.modules.helper_funcs.alternate import send_message
+from FallenRobot.modules.helper_funcs.chat_status import is_user_admin
+from FallenRobot.modules.helper_funcs.extraction import (
     extract_unt_fedban,
     extract_user,
     extract_user_fban,
 )
-from MukeshRobot.modules.helper_funcs.string_handling import markdown_parser
+from FallenRobot.modules.helper_funcs.string_handling import markdown_parser
 
 
 
